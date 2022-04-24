2d8d996b5c5a327e8c61a8002b91542149f7da4c
mrdotx <klassiker@gmx.de>
2022-04-24 08:16:28 +0200

    comment changed

--- memtest86.sh
+++ memtest86.sh
@@ -3,9 +3,9 @@
 # path:   /home/klassiker/.local/share/repos/efistub/memtest86.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2022-04-21T10:42:49+0200
+# date:   2022-04-24T08:00:42+0200
 
-# speed up script by using posix
+# speed up script by using standard c
 LC_ALL=C
 LANG=C
 
